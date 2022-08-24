1cb2fe7fa1cd5790c01d4a81436ce39f02ee7b6a
kauthar <kowsardalmar@gmail.com>
2022-08-24 11:52:28 +0300

    Heart Trail Animation

---
+++ script.js
@@ -0,0 +1,23 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove",(event)=>{
+    const xpos = event.offsetX;
+    const ypos = event.offsetY;
+
+    const spanEl = document.createElement("span");
+
+    spanEl.style.left = xpos + "px"
+    spanEl.style.top = ypos + "px"
+    bodyEl.appendChild(spanEl);
+    const size  = Math.random()*100
+    spanEl.style.width =  size + "px"
+    spanEl.style.height =  size + "px"
+
+    setTimeout(()=>{
+        spanEl.remove()
+
+    }, 3000)
+
+})
+
+
